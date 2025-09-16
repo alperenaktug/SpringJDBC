2b0ff5ef48e4862988e0fa9dce50ef2e54b695ec
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-09-16 23:48:32 +0300

    Student service and repository

--- src/main/java/com/telusko/SpringJDBC/SpringJdbcApplication.java
+++ src/main/java/com/telusko/SpringJDBC/SpringJdbcApplication.java
@@ -1,9 +1,12 @@
 package com.telusko.SpringJDBC;
 
 import com.telusko.SpringJDBC.model.Student;
+import com.telusko.SpringJDBC.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
@@ -11,13 +14,19 @@ public class SpringJdbcApplication {
 	public static void main(String[] args) {
 		ApplicationContext context =  SpringApplication.run(SpringJdbcApplication.class, args);
 
-		Student student = context.getBean(Student.class);
+		Student s = context.getBean(Student.class);
 
-		student.setRollNo(101);
-		student.setName("Polat");
-		student.setMarks(78);
+		s.setRollNo(101);
+		s.setName("Polat");
+		s.setMarks(78);
 
-		
+		StudentService service = context.getBean(StudentService.class);
+
+		service.addStudent(s);
+
+		List<Student> students = service.getStudents();
+
+		System.out.println(students);
 
 	}
 
---
+++ src/main/java/com/telusko/SpringJDBC/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.telusko.SpringJDBC.service;
+
+import com.telusko.SpringJDBC.model.Student;
+import com.telusko.SpringJDBC.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+
+    public StudentRepo repo ;
+
+    public StudentRepo getRepo() {
+        return repo;
+    }
+
+    @Autowired
+    public void setRepo(StudentRepo repo) {
+        this.repo = repo;
+    }
+
+    public void addStudent(Student s) {
+       repo.save(s);
+    }
+
+    public List<Student> getStudents() {
+        return repo.findAll();
+    }
+}
